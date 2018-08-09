fd37a5c5731408511315e8b9f15b7ac221dd31a5
shikibum <satoshikibu@gmail.com>
2018-08-09 23:33:12 +0900

    delete, edit機能実装

--- import.sql
+++ import.sql
@@ -1,8 +1,8 @@
 create table memos(
-    id integer primary key,
-    title not null,
+    id integer primary key autoincrement,
+    title text not null,
     body text not null 
 );
-insert into memos(id, title, body) values
-(1 ,'memo1', 'test1'),
-(2, 'memo2', 'test2');
+insert into memos(title, body) values
+('memo1', 'test1'),
+('memo2', 'test2');
--- main.rb
+++ main.rb
@@ -1,16 +1,53 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
 
-get '/' do 
-    @title = 'メモアプリ'
-    @content = 'main content'
-    erb :index
+ActiveRecord::Base.establish_connection(
+  'adapter' => 'sqlite3',
+  'database' => './memos.db'
+)
+
+use Rack::MethodOverride
+
+class Memo < ActiveRecord::Base
+end
+
+after do
+  ActiveRecord::Base.connection.close
+end
+
+get '/' do
+  @memos = Memo.order('id desc').all
+  erb :index
 end
 
 get '/new' do
   erb :new
 end
 
-get '/edit' do
+post "/new" do 
+    Memo.create(title: params['title'], body: params['body'])
+    redirect '/'
+    erb :new
+end
+
+get '/memos/:id/edit' do
+  @memo = Memo.find(params['id'])
   erb :edit
+end
+
+patch '/memos/:id/edit' do
+  @memo = Memo.find(params['id'])
+  @memo.update(title: params['title'], body: params['body'])
+  erb :show
+end
+
+get "/memos/:id" do
+  @memo = Memo.find(params['id'])
+  erb :show
+end
+
+delete '/memos/:id' do
+  Memo.find(params['id']).destroy
+  redirect '/'
 end
